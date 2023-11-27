86261c1e332fc53f9ad6f60d0e686e45910abb6e
David Pérez <deividpa022@gmail.com>
2023-11-27 18:57:20 -0500

    foldor controller created

---
+++ src/main/java/com/dpa/news/controllers/NewsController.java
@@ -0,0 +1,14 @@
+package com.dpa.news.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author David Perez
+ */
+@Controller
+@RequestMapping("/news")
+public class NewsController {
+    
+}
