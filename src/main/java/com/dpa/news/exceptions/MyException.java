10675ee68c98b41deb398299af1262bc05f5d5f8
David Pérez <deividpa022@gmail.com>
2023-11-27 18:58:47 -0500

    MyException created in order to personalize the errors in the services

---
+++ src/main/java/com/dpa/news/exceptions/MyException.java
@@ -0,0 +1,11 @@
+package com.dpa.news.exceptions;
+
+/**
+ *
+ * @author David Perez
+ */
+public class MyException extends Exception {
+    public MyException(String msg) {
+        super(msg);
+    }
+}
