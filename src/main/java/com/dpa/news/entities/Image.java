265502e01bead4c5f4ced58f4a854a0aff05621d
David Pérez <deividpa022@gmail.com>
2023-12-12 02:05:37 -0500

    Entity Image for store the Users Images

---
+++ src/main/java/com/dpa/news/entities/Image.java
@@ -0,0 +1,67 @@
+package com.dpa.news.entities;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ *
+ * @author David Perez
+ */
+@Entity
+public class Image {
+    
+    public Image() {
+    }
+    
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    
+    private String mime;
+    private String name;
+
+    
+    
+    @Lob @Basic(fetch = FetchType.LAZY)
+    private byte[] content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMime() {
+        return mime;
+    }
+
+    public void setMime(String mime) {
+        this.mime = mime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+    
+    
+}
